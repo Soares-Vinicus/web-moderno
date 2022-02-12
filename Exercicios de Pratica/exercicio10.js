8ba350c1114f80fb517bdddb1df42478d17f8b17
Vinícius Soares <viniciussou222@gmail.com>
2022-02-12 03:12:14 -0300

    Create exercicio10.js

---
+++ Exercicios de Pratica/exercicio10.js
@@ -0,0 +1,9 @@
+/*
+Elabore uma função que recebe um número como parâmetro e retorne uma string
+com o símbolo "+" na quantidade especificada no parâmetro.
+*/
+function simboloMais (num1){
+    let mais= "+"
+    console.log(mais.repeat(num1))
+}
+simboloMais(4)
