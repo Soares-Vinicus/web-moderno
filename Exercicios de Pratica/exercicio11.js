f46aaf7a5d0b03ea141dd7870b460338b1603b34
Vinícius Soares <viniciussou222@gmail.com>
2022-02-12 03:26:51 -0300

    Create exercicio11.js

---
+++ Exercicios de Pratica/exercicio11.js
@@ -0,0 +1,9 @@
+/*
+Crie uma função que receba uma array e retorne o primeiro e o último elemento 
+desse array como um novo array:
+*/
+let vetor = [4,5,6]
+function PrimeeUltimo(vetor){
+    console.log(vetor[0], vetor[vetor.length -1])
+}
+PrimeeUltimo(vetor)
