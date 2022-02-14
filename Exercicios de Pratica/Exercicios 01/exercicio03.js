7e49848ac32ddd36e1f7246dee2f0c42a28d38d8
Vinícius Soares <viniciussou222@gmail.com>
2022-02-14 09:28:06 -0300

    exercicio04.js

---
+++ Exercicios de Pratica/Exercicios 01/exercicio03.js
@@ -0,0 +1,7 @@
+/*
+Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+*/
+function potencia(num1, num2){
+    console.log(num1 ** num2)
+}
+potencia(2,2)
---
+++ Exercicios de Pratica/Exercicios 01/exercicio04.js
@@ -0,0 +1,8 @@
+/*
+Crie uma função que irá receber dois valores, o dividendo e o divisor. A função
+deverá imprimir o resultado e o resto da divisão destes dois valores.
+*/
+function divisao(num1, num2){
+    console.log(num1/num2)
+}
+divisao(16,4)
