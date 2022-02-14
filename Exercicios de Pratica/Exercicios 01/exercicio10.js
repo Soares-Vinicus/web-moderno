aa4ca150c1f7a34bebc772a0bb9f221cbf94fcb4
Vinícius Soares <viniciussou222@gmail.com>
2022-02-14 19:15:39 -0300

    Create exercicio10.js

---
+++ Exercicios de Pratica/Exercicios 01/exercicio10.js
@@ -0,0 +1,8 @@
+/*
+ Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
+ou false.
+*/
+function div3(n1){
+    console.log( n1 % 3 == 0)
+}
+div3(27)
